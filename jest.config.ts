0531c02ca6e923e6cf51cff0d9c697ae269daf5c
Vitor Neves <vitornvj@gmail.com>
2021-09-13 19:33:10 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,10 @@ export default {
   roots: [
     '<rootDir>/src'
   ],
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   preset: 'ts-jest',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
