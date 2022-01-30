47cc9908b034a2031b66a6e4fa912d98e9b701e0
Vitor Varjão <vitornvj@gmail.com>
2022-01-30 18:26:31 -0300

    feat: add login factory

--- src/main/factories/controllers/authentication.ts
+++
@@ -1,6 +0,0 @@
-import { LoginController } from '../../../presentation/controllers/login/login-controller'
-import { makeAuthentication } from '../services/authentication'
-
-export const makeAuthenticationController = () => {
-  return new LoginController(makeAuthentication())
-}
---
+++ src/main/factories/controllers/login-controller-factory.ts
@@ -0,0 +1,12 @@
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import { makeAuthentication } from '../services/authentication'
+import { makeLoginValidation } from './login-validation-factory'
+
+export const makeAuthenticationController = (): Controller => {
+  const loginController = new LoginController(makeLoginValidation(), makeAuthentication())
+  const logErrorRepository = new LogMongoRepository()
+  return new LogControllerDecorator(loginController, logErrorRepository)
+}
