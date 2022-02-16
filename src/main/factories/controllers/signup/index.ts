de429409dbe1115235650afcaa3bb2cb337a72c2
Vitor Varjão <vitornvj@gmail.com>
2022-02-15 21:09:38 -0300

    refactor: separate factories in folders and inject decorator in controller factories

--- src/main/factories/controllers/signup/index.ts
+++ src/main/factories/controllers/signup/index.ts
@@ -1,13 +1,11 @@
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
 import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
 import { Controller } from '../../../../presentation/protocols'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
+import { makeLogController } from '../../decorators/log-controller-decorator-factory'
 import { makeAuthentication } from '../../services/authentication'
 import { makeAddAccount } from '../../services/signup'
 import { makeSignUpValidation } from './signup-validation-factory'
 
 export const makeSignUpController = (): Controller => {
-  const logMongoRepository = new LogMongoRepository()
   const signUpController = new SignUpController(makeAddAccount(), makeSignUpValidation(), makeAuthentication())
-  return new LogControllerDecorator(signUpController, logMongoRepository)
+  return makeLogController(signUpController)
 }
