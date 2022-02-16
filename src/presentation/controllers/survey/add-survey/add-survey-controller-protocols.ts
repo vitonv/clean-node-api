30bdf2fef672c8ca5dd3e11557972db745a5a6af
Vitor Varjão <vitornvj@gmail.com>
2022-02-16 01:30:20 -0300

    feat: ensure AddSurveyController calls AddSurvey with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../helpers/http-helpers'
 export * from '../../../protocols'
 export * from '../../../../validation/validators'
+export * from '../../../../domain/usecases/add-survey'
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,10 +1,19 @@
-import { badRequest, Controller, HttpRequest, HttpResponse, serverError, Validation } from './add-survey-controller-protocols'
+import { AddSurvey, badRequest, Controller, HttpRequest, HttpResponse, serverError, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body)
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({
+        question,
+        answers
+      })
       if (error) {
         return badRequest(error)
       }
